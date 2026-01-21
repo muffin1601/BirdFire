3a049242f1faf33fb86490bbd2a6d1a696b8b96a
muffin1601 <sana38790@gmail.com>
2026-01-21 17:09:26 +0530

    first push

--- birdfire-frontend/src/app/layout.tsx
+++ birdfire-frontend/src/app/layout.tsx
@@ -1,4 +1,11 @@
 import "./globals.css"
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BirdFire",
+  description: "BirdFire Furniture",
+};
+
 
 export default function RootLayout({
     children,
